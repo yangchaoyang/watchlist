1f5c496373a4c6c563ce86f6c5d46169338aecda
yangchaoyang <cy_yang9090@163.com>
2020-08-20 00:39:56 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,59 @@
+import os
+import sys
+
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 # from flask import url_for, escape
 
-app = Flask(__name__)
+# WIN = sys.platform.startswith('win')
+# if WIN:     #如果是Windows系统，使用三个斜线
+#     prefix = 'sqlite:///'
+# else:       #否则使用四个斜线
+#     prefix = 'sqlite:////'
 
-name = 'yangchaoyang'
-movies = [
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    #关闭对模型修改的监控
+#在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user = user, movies = movies)
+
+# 创建数据库模型
+class User(db.Model):       #表名将会是user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)    #主键
+    name = db.Column(db.String(20))     #名字
+
+class Movie(db.Model):      #表名将会是movie
+    id = db.Column(db.Integer, primary_key=True)    #主键
+    title = db.Column(db.String(60))    #电影标题
+    year = db.Column(db.String(4))      #电影年份
+
+#自定义命令
+import click
+
+@app.cli.command()      #注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')    #设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:    #判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialize database.')      #输出提示信息
+
+#自定义命令把虚拟数据添加到数据库里
+@app.cli.command()
+def forge():
+    """Generate fake date."""
+    db.create_all()
+
+    name = 'yangchaoyang'
+    movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
     {'title': 'Dead Poets Society', 'year': '1989'},
     {'title': 'A Perfect World', 'year': '1993'},
@@ -16,28 +64,13 @@ movies = [
     {'title': 'Devils on the Doorstep', 'year': '1999'},
     {'title': 'WALL-E', 'year': '2008'},
     {'title': 'The Pork of Music', 'year': '2012'},
-]
+    ]
 
-@app.route('/')
-def index():
-    return render_template('index.html', name=name, movies=movies)
-# @app.route('/')
-# def hello():
-#     return 'Hello'
-
-# @app.route('/user/<name>')
-# def user_page(name):
-#     return 'User: %s' % escape(name)
-
-# @app.route('/test')
-# def test_url_for():
-#     print(url_for('hello'))
-#     print(url_for('user_page', name='ycy'))
-#     print(url_for('user_page', name='cy'))
-#     print(url_for('test_url_for'))
-#     print(url_for('test_url_for', num=2))
-#     return 'Test page'
-
-
-
-
+    user = User(name = name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    
+    db.session.commit()
+    click.echo('Done')
